c04e316692381b48ed058add7b3cdd8841e13df8
vasudev-san <sanklavasudev@gmail.com>
2023-06-22 23:03:03 +0530

    listview added

--- app/src/main/java/com/fatbit/ieltsexamprep/MainActivity.java
+++ app/src/main/java/com/fatbit/ieltsexamprep/MainActivity.java
@@ -2,13 +2,28 @@ package com.fatbit.ieltsexamprep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button speaking,listening,reading,writing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        speaking = findViewById(R.id.speaking);
+        listening = findViewById(R.id.listening);
+        reading = findViewById(R.id.reading);
+        writing = findViewById(R.id.writing);
+
+        reading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ReadingMainPage.class));
+            }
+        });
     }
 }
