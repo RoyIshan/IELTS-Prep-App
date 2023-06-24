ca6a19873a9191ad154f8d8157f6c6f05eb74a7a
Ishan Roy <78752693+RoyIshan@users.noreply.github.com>
2023-06-24 22:02:40 +0530

    Speaking 2.1

--- app/src/main/java/com/fatbit/ieltsexamprep/Speaking/Part1/SpeakingPart1EachContent.java
+++ app/src/main/java/com/fatbit/ieltsexamprep/Speaking/Part1/SpeakingPart1EachContent.java
@@ -2,26 +2,23 @@ package com.fatbit.ieltsexamprep.Speaking.Part1;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fatbit.ieltsexamprep.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 public class SpeakingPart1EachContent extends AppCompatActivity {
 
-    TextView q1,q2,q3,q4,q5,ans1View,ans2View,ans3View,ans4View,ans5View;
+    TextView q1,q2,q3,q4,q5,ans1View,ans2View,ans3View,ans4View,ans5View, textView2;
     FirebaseFirestore db;
     String paraNo;
-    Button ans;
+    //Button ans;
+    ToggleButton ToggleAnswer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,49 +36,93 @@ public class SpeakingPart1EachContent extends AppCompatActivity {
         ans3View = findViewById(R.id.ans3View);
         ans4View = findViewById(R.id.ans4View);
         ans5View = findViewById(R.id.ans5View);
-        ans = findViewById(R.id.ans);
+        textView2 = findViewById(R.id.textView2);
+        //ans = findViewById(R.id.ans);
+        ToggleAnswer = findViewById(R.id.toggleButton);
+        ToggleAnswer.setChecked(false);
+
+        ans1View.setVisibility(View.GONE);
+        ans2View.setVisibility(View.GONE);
+        ans3View.setVisibility(View.GONE);
+        ans4View.setVisibility(View.GONE);
+        ans5View.setVisibility(View.GONE);
 
         db = FirebaseFirestore.getInstance();
         db.collection("Reading Part1 Topics")
                 .whereEqualTo(FieldPath.documentId(),paraNo)
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                q1.setText(document.getString("Q1"));
-                                q2.setText(document.getString("Q2"));
-                                q3.setText(document.getString("Q3"));
-                                q4.setText(document.getString("Q4"));
-                                q5.setText(document.getString("Q5"));
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            textView2.setText(document.getId());
+                            q1.setText(document.getString("Q1"));
+                            q2.setText(document.getString("Q2"));
+                            q3.setText(document.getString("Q3"));
+                            q4.setText(document.getString("Q4"));
+                            q5.setText(document.getString("Q5"));
 
-                            }
                         }
                     }
                 });
-        ans.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                db.collection("Reading Part1 Topics")
-                        .whereEqualTo(FieldPath.documentId(),paraNo)
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                        ans1View.setText(document.getString("Ans1"));
-                                        ans2View.setText(document.getString("Ans2"));
-                                        ans3View.setText(document.getString("Ans3"));
-                                        ans4View.setText(document.getString("Ans4"));
-                                        ans5View.setText(document.getString("Ans5"));
-                                    }
-                                }
+
+        /*ans.setOnClickListener(v -> {
+
+            ans1View.setVisibility(View.VISIBLE);
+            ans2View.setVisibility(View.VISIBLE);
+            ans3View.setVisibility(View.VISIBLE);
+            ans4View.setVisibility(View.VISIBLE);
+            ans5View.setVisibility(View.VISIBLE);
+
+            db.collection("Reading Part1 Topics")
+                    .whereEqualTo(FieldPath.documentId(),paraNo)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                ans1View.setText(document.getString("Ans1"));
+                                ans2View.setText(document.getString("Ans2"));
+                                ans3View.setText(document.getString("Ans3"));
+                                ans4View.setText(document.getString("Ans4"));
+                                ans5View.setText(document.getString("Ans5"));
                             }
-                        });
-            }
-        });
+                        }
+                    });
+        });*/
+    }
+
+    public void onToggleClick(View view)
+    {
+        if (ToggleAnswer.isChecked()) {
+            ans1View.setVisibility(View.VISIBLE);
+            ans2View.setVisibility(View.VISIBLE);
+            ans3View.setVisibility(View.VISIBLE);
+            ans4View.setVisibility(View.VISIBLE);
+            ans5View.setVisibility(View.VISIBLE);
+
+            db.collection("Reading Part1 Topics")
+                    .whereEqualTo(FieldPath.documentId(),paraNo)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                ans1View.setText(document.getString("Ans1"));
+                                ans2View.setText(document.getString("Ans2"));
+                                ans3View.setText(document.getString("Ans3"));
+                                ans4View.setText(document.getString("Ans4"));
+                                ans5View.setText(document.getString("Ans5"));
+                            }
+                        }
+                    });
+        }
+        else {
+
+            ans1View.setVisibility(View.GONE);
+            ans2View.setVisibility(View.GONE);
+            ans3View.setVisibility(View.GONE);
+            ans4View.setVisibility(View.GONE);
+            ans5View.setVisibility(View.GONE);
+
+        }
     }
 
 }
--- app/src/main/java/com/fatbit/ieltsexamprep/Speaking/Part1/SpeakingPart1Topics.java
+++ app/src/main/java/com/fatbit/ieltsexamprep/Speaking/Part1/SpeakingPart1Topics.java
@@ -2,22 +2,15 @@ package com.fatbit.ieltsexamprep.Speaking.Part1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fatbit.ieltsexamprep.R;
-import com.fatbit.ieltsexamprep.ReadingMCQEachPara;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -41,32 +34,25 @@ public class SpeakingPart1Topics extends AppCompatActivity {
 
         db.collection("Reading Part1 Topics")
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                //data.add(document.getString("Name"));
-                                data.add(document.getId());
-                                listView.setAdapter(adapter);
-                            }
-                        } else {
-                            Toast.makeText(SpeakingPart1Topics.this,"Ni chal ra",Toast.LENGTH_SHORT).show();
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            data.add(document.getId());
+                            listView.setAdapter(adapter);
                         }
+                    } else {
+                        Toast.makeText(SpeakingPart1Topics.this,"Error",Toast.LENGTH_SHORT).show();
                     }
                 });
         // Set the adapter to the ListView
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = adapter.getItem(position);
-                Intent intent = new Intent(SpeakingPart1Topics.this, SpeakingPart1EachContent.class);
-                intent.putExtra("paraNo",selectedItem);
-                startActivity(intent);
-                Toast.makeText(SpeakingPart1Topics.this, selectedItem + " clicked", Toast.LENGTH_SHORT).show();
-            }
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedItem = adapter.getItem(position);
+            Intent intent = new Intent(SpeakingPart1Topics.this, SpeakingPart1EachContent.class);
+            intent.putExtra("paraNo",selectedItem);
+            startActivity(intent);
+            Toast.makeText(SpeakingPart1Topics.this, selectedItem + " clicked", Toast.LENGTH_SHORT).show();
         });
     }
 
